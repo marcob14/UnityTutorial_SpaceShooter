1a9c4e896a4670d9fe86e54fdcba5c4ec152f1b8
Marco Borg <marcob14@gmail.com>
2015-12-11 00:46:06 +0100

    Creating hazards
    Create an asteroid hazard to challenge the player.
    http://unity3d.com/learn/tutorials/projects/space-shooter/creating-hazards?playlist=17147

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.tag == "Boundary")
+            return;
+
+        Destroy(obj.gameObject);
+        Destroy(gameObject);
+    }
+}
