5d32e7e6fa8b2f429a73d362efe9b24f8eced47d
Marco Borg <marcob14@gmail.com>
2015-12-12 21:52:01 +0100

    Explosions
    Add explosions to the scene when hazards or the player is destroyed.
    http://unity3d.com/learn/tutorials/projects/space-shooter/explosions?playlist=17147

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider obj)
     {
         if (obj.tag == "Boundary")
             return;
+		 
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		if(obj.tag == "Player")
+			Instantiate(playerExplosion, obj.transform.position, obj.transform.rotation);
 
         Destroy(obj.gameObject);
         Destroy(gameObject);
