8e7551f8f0cd80322016b3335848b36c41f8588b
Marco Borg <marcob14@gmail.com>
2015-12-14 22:02:44 +0100

    Points system
    Counting points and displaying the score.
    http://unity3d.com/learn/tutorials/projects/space-shooter/counting-points?playlist=17147

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,6 +5,18 @@ public class DestroyByContact : MonoBehaviour {
 
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start() {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+
+		if (gameControllerObject)
+			gameController = gameControllerObject.GetComponent<GameController>();
+
+		if (gameController == null)
+			Debug.Log ("Cannot find 'GameController' script");
+	}
 
     void OnTriggerEnter(Collider obj)
     {
@@ -13,8 +25,10 @@ public class DestroyByContact : MonoBehaviour {
 		 
 		Instantiate(explosion, transform.position, transform.rotation);
 
-		if(obj.tag == "Player")
-			Instantiate(playerExplosion, obj.transform.position, obj.transform.rotation);
+		if (obj.tag == "Player")
+			Instantiate (playerExplosion, obj.transform.position, obj.transform.rotation);
+
+		gameController.AddScore (scoreValue);
 
         Destroy(obj.gameObject);
         Destroy(gameObject);
